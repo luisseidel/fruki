692753dbf514aa1f8336081b0286c1f13c390cc8
Luis Seidel <luis.seidel@celk.net>
2023-03-29 19:24:01 -0300

    fix: configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,25 @@ const colors = require('tailwindcss/colors')
 
 
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
-  ],
-  purge: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
-  ],
-  darkMode: false,
-  theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-      },
+    content: [
+        "./src/**/*.{html,js}",
+        "./src/app/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}"
+    ],
+    purge: [
+        "./src/app/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}"
+    ],
+    darkMode: false,
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
