a6be8e13f11a6175426a45bb6f5772462ad6dd7e
Luis Seidel <luis.seidel@celk.net>
2023-03-30 20:16:24 -0300

    gogo

--- src/app/components/Video.tsx
+++
@@ -1,7 +0,0 @@
-export default function Video() {
-    return (
-        <div className="w-screen h-auto">
-            <video src={'https://fruki.com.br/videos/industria.mp4'} width="" height="" controls={true} autoPlay={true} />
-        </div>
-    );
-}
